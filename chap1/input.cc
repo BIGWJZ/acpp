25a62c87d80c27dc2d90e64286800df895e508aa
BIGWJZ <bigwjz@outlook.com>
2022-07-05 21:20:07 +0800

    compelete chap1

--- chap1/input.cc
+++ chap1/input.cc
@@ -5,6 +5,14 @@ int main(){
     std::cout << "Please enter your first name: " ;
     std::string name;
     std::cin >> name;
-    std::cout << "Hello, " << name << "!" << std::endl;
+    const std::string greetings = " Hello, " + name + "! ";
+    const std::string spaces(greetings.size(),' ');
+    const std::string second = "*" + spaces + "*";
+    const std::string first(second.size(),'*');
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "*" << greetings << "*" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
     return 0;
 }
